462b5feda7bc9e9f79501c12bc571b8938ecd6be
unknown <janhavijadhav8013@gmail.com>
2024-01-31 13:32:27 +0530

    commit

---
+++ Function.java
@@ -0,0 +1,35 @@
+package Java_Ex;
+
+import java.util.Scanner;
+
+public class Function {
+	 int r = 9;
+	    int v = 4;
+	   Function(){
+	       System.out.println("Constructor Called");
+	   }
+	   void callFunction(){
+	       System.out.println("Function Called");
+	   }
+	   void add(int a, int b){
+	       int c= a+b;
+	       System.out.println("Addition of " + a + " and " + b +" is " + c);
+	   }
+	   int mul(int p, int q){
+	       return  p*q;
+	   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x , y;
+	       Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter Two numbers for addition : ");
+	       x = sc.nextInt();
+	       y = sc.nextInt();
+	        Function f = new Function();
+	        f.callFunction();
+	        //f.add(6,7);
+	        f.add(x , y);
+	        System.out.println("Multiplication of "+ f.r+ " and "+ f.v+ " is "+ f.mul(f.r, f.v));
+	}
+
+}
