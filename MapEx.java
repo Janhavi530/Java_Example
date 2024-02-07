f8e464beb68e98e92d0486bd69011c0c495b35dd
unknown <janhavijadhav8013@gmail.com>
2024-02-07 10:32:05 +0530

    commit

---
+++ MapEx.java
@@ -0,0 +1,18 @@
+package Java_Ex;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapEx {
+
+	public static void main(String[] args) {
+		Map<Integer,String> m=new HashMap<>();
+		m.put(101,"A");
+		m.put(102, "B");
+		System.out.println(m);
+		System.out.println("Entry:"+m.entrySet());
+		System.out.println("Key:"+m.keySet());
+		System.out.println("Values:"+m.values());
+	}
+
+}
